93d131970ef49021b635a8e35b0e84128dc669f0
shotgunmech <47787927+shotgunmech@users.noreply.github.com>
2020-09-20 05:24:54 -0400

    implimenting material-ui

--- src/index.js
+++ src/index.js
@@ -2,45 +2,58 @@ import React from "react";
 import ReactDOM from "react-dom";
 import User from "./User";
 import axios from "axios";
+import MaterialTable from "material-table";
 
 // 'Outer' component that will contain all the User 'cards'
 class UserGrid extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = { users: [] };
-  }
+        this.state = { users: [] };
+    }
 
-  // Runs when component is mounted
-  componentDidMount() {
-    // Get data for 100 users
-    axios
-      .get("https://randomuser.me/api/?results=100")
-      .then(response => {
-          console.log(response)
-        // GET request was successful, store the users in state
-        this.setState({ users: response.data.results });
-      })
-      .catch(err => {
-        // GET failed, log the error
-        console.log(err);
-      });
-  }
+    // Runs when component is mounted
+    componentDidMount() {
+        // Get data for 100 users { users: response.data.results }
+        axios
+            .get("https://randomuser.me/api/?results=100")
+            .then(response => {
+                console.log(response.data.results)
+                this.setState({ users: response.data.results });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
 
-  render() {
-    const userList = this.state.users.map(u => (
-      <User
-        key={u.name.first}
-        name={u.name.first}
-        last={u.name.last}
-        age={u.dob.age}
-        image={u.picture.medium}
-        quote={u.quote}
-      />
-    ));
-
-    return <div className="columns is-multiline">{userList}</div>;
-  }
+    render() {
+        const userList = this.state.users.map(u => (
+            <User
+                key={u.email}
+                name={u.name.first}
+                last={u.name.last}
+                age={u.dob.age}
+                image={u.picture.medium}
+                quote={u.quote}
+            />
+        ));
+        console.log(this.state.users.age)
+        return (
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Photo</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Age</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {userList}
+                </tbody>
+            </table>
+        )
+    }
 }
 
 ReactDOM.render(<UserGrid />, document.getElementById("root"));
